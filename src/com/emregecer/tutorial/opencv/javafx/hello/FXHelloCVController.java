9db57d36cdc54700c841be639beae57153349af4
emre <emre@caunos.com>
2018-01-22 12:59:43 +0300

    First commit for openCV javaFX tutorial

---
+++ src/com/emregecer/tutorial/opencv/javafx/hello/FXHelloCVController.java
@@ -0,0 +1,25 @@
+package com.emregecer.tutorial.opencv.javafx.hello;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class FXHelloCVController {
+	
+	@FXML
+	private Button button;
+	
+	@FXML
+	private ImageView currentFrame;
+	
+	@FXML
+	public void startCamera(ActionEvent event) {
+		
+	}
+	
+	public void setClosed() {
+
+	}
+	
+}
